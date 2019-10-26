34161103c4628eb046df16cce4e27077b29ecee8
Tom König <hi@tomknig.de>
2019-10-26 10:47:15 +0200

    Remove express specific https config

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const { setup } = require('radiks-server')
-const https = require('https')
 const fs = require('fs')
 
-const developmentConfig = {
-  key: fs.readFileSync('./key.dev.pem'),
-  cert: fs.readFileSync('./cert.dev.pem'),
-  passphrase: 'foobar',
-}
-const productionConfig = {}
-const config = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig
-
 const app = express()
-app.server = https.createServer(config, app)
 
 setup().then(RadiksController => {
   app.use('/radiks', RadiksController)
