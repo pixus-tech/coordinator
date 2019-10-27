660256911678b75a3ab2c1e151b1a29814b1b69b
Tom König <hi@tomknig.de>
2019-10-27 23:31:23 +0100

    Update cert setup

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Development
 
 - Add `127.0.0.1 pixus.local` to your `/etc/hosts` file.
-- Generate certificates for local development with`./bin/generate-certificates.sh`
+- Generate certificates for local development
+  with`./bin/generate-certificates.sh` and set `pixus` as the password.
 - Start the server with `docker-compose up`
--- bin/generate-certificates.sh
+++ bin/generate-certificates.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -out config/pixus.crt -keyout config/pixus.key \
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=pixus.local' -extensions EXT -config <( \
-   printf "[dn]\nCN=pixus.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:pixus.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+openssl req -x509 -newkey rsa:4096 -keyout config/pixus.key -out config/pixus.crt -days 365
