12a4f48c8fd50601f947e5b7e58e36d8ce24c399
Tom König <hi@tomknig.de>
2019-10-28 23:25:14 +0100

    Add sentry

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const Sentry = require('@sentry/node')
 const { setup } = require('radiks-server')
 const fs = require('fs')
+const isProduction = process.env.NODE_ENV
 
 const app = express()
+
+if (isProduction) {
+  Sentry.init({ dsn: process.env.SENTRY_DSN })
+  app.use(Sentry.Handlers.requestHandler())
+}
 
 setup().then(RadiksController => {
   app.use('/radiks', RadiksController)
@@ -14,5 +21,9 @@ app.get('/ping', function (req, res) {
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
   res.json({ status: 'alive' })
 })
+
+if (isProduction) {
+  app.use(Sentry.Handlers.errorHandler())
+}
 
 module.exports = app
